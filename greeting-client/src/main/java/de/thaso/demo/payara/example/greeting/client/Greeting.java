7c6f73a352ef616c7a88cbc492a716509d77616f
athaler <athaler001@thaso.de>
2023-03-26 15:38:32 +0200

    Client-Rest-Call mit mTLS

--- greeting-client/src/main/java/de/thaso/demo/payara/example/greeting/client/Greeting.java
+++ greeting-client/src/main/java/de/thaso/demo/payara/example/greeting/client/Greeting.java
@@ -3,9 +3,6 @@ package de.thaso.demo.payara.example.greeting.client;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -15,17 +12,17 @@ public class Greeting {
     private final static Logger LOGGER = LoggerFactory.getLogger(Greeting.class);
 
     public static void main(final String[] args) throws NoSuchAlgorithmException {
-        System.setProperty("https.protocols", "TLSv1.2");
+        //System.setProperty("https.protocols", "TLSv1.2");
 
-//        System.setProperty("javax.net.ssl.keyStoreType", "jks");
-//        System.setProperty("javax.net.ssl.keyStore", "/home/mp008342/demo/payara/GreetingPlus/CA/serverstore.jks");
-//        System.setProperty("javax.net.ssl.keyStorePassword", "geheim123");
+        System.setProperty("javax.net.ssl.keyStoreType", "jks");
+        System.setProperty("javax.net.ssl.keyStore", "./CA/mTLS/keystore.jks");
+        System.setProperty("javax.net.ssl.keyStorePassword", "changeit");
 
         System.setProperty("javax.net.ssl.trustStoreType", "jks");
         System.setProperty("javax.net.ssl.trustStore", "./CA/payara/truststore.jks");
         System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
 
-        System.setProperty("javax.net.debug", "ssl");
+        //System.setProperty("javax.net.debug", "ssl");
 
         final Greeting greeting = new Greeting();
         System.out.println("Calling: " + greeting.callGreeting());
--- greeting-client/src/test/java/de/thaso/demo/payara/example/greeting/client/GreetingTest.java
+++ greeting-client/src/test/java/de/thaso/demo/payara/example/greeting/client/GreetingTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 
-import java.security.NoSuchAlgorithmException;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.MockitoAnnotations.openMocks;
@@ -18,17 +16,17 @@ class GreetingTest {
     void setUp() {
         openMocks(this);
 
-        System.setProperty("https.protocols", "TLSv1.2");
+        //System.setProperty("https.protocols", "TLSv1.2");
 
-//        System.setProperty("javax.net.ssl.keyStoreType", "jks");
-//        System.setProperty("javax.net.ssl.keyStore", "/home/mp008342/demo/payara/GreetingPlus/CA/serverstore.jks");
-//        System.setProperty("javax.net.ssl.keyStorePassword", "geheim123");
+        System.setProperty("javax.net.ssl.keyStoreType", "jks");
+        System.setProperty("javax.net.ssl.keyStore", "../CA/mTLS/keystore.jks");
+        System.setProperty("javax.net.ssl.keyStorePassword", "changeit");
+
         System.setProperty("javax.net.ssl.trustStoreType", "jks");
         System.setProperty("javax.net.ssl.trustStore", "../CA/payara/truststore.jks");
         System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
 
         System.setProperty("javax.net.debug", "ssl");
-
     }
 
     @InjectMocks
